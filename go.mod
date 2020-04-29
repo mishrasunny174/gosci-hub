c254c99afc194d62dda8deef1ce428a5dba20d67
Sunny Mishra <mishrasunny174@gmail.com>
2020-04-29 23:08:16 +0530

    renamed library files

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/mishrasunny174/gosci-hub
 
 go 1.14
 
-require github.com/PuerkitoBio/goquery v1.5.1
+require (
+	github.com/PuerkitoBio/goquery v1.5.1
+	github.com/mishrasunny174/gosci-hub/libgoscihub v0.0.0-20200429173224-3ae29d468c2b // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"log"
+)
 
+func main() {
+	pdfBuff, err := libgoscihub.GetPdfFromArticleURL("https://link.springer.com/referenceworkentry/10.1007%2F978-1-4419-1428-6_634")
+	if err != nil {
+		log.Println(err)
+	}
+	log.Printf("%v\n", pdfBuff)
 }
