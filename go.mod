a3b21c8550227e77aead2c6828af3de7fa432470
Sunny Mishra <mishrasunny174@gmail.com>
2020-05-01 10:18:50 +0530

    updated libgosci hub tests to account for some url's which doesn't contain https: in the begining

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.14
 
 require (
 	github.com/dustin/go-humanize v1.0.0
-	github.com/mishrasunny174/gosci-hub/libdownloader v0.0.0-20200430054041-88de87fb2949
-	github.com/mishrasunny174/gosci-hub/libgoscihub v0.0.0-20200430052657-bb2df5fc2788
+	github.com/mishrasunny174/gosci-hub/libdownloader v0.0.0-20200430071827-6a6553cd589f
+	github.com/mishrasunny174/gosci-hub/libgoscihub v0.0.0-20200430071827-6a6553cd589f
 	golang.org/x/net v0.0.0-20200425230154-ff2c4b7c35a0 // indirect
 )
--- libgoscihub/libgoscihub_test.go
+++ libgoscihub/libgoscihub_test.go
@@ -10,13 +10,14 @@ func TestGetPDFURL(t *testing.T) {
 		"https://link.springer.com/referenceworkentry/10.1007%2F978-1-4419-1428-6_634",
 		"https://www.researchgate.net/publication/282284055_When_Every_Byte_Counts_-_Writing_Minimal_Length_Shellcodes",
 		"https://sci-hub.tw/https://www.researchgate.net/publication/261123624_JOP-alarm_Detecting_jump-oriented_programming-based_anomalies_in_applications",
+		"https://www.researchgate.net/publication/324558339_Predicting_Cyber_Events_by_Leveraging_Hacker_Sentiment",
 	}
 	for _, inp := range inps {
 		got, err := GetPDFURL(inp)
 		if err != nil {
 			t.Fatalf("Unknow error occured\nReturned value: %s\nERROR: %v\n", got, err)
 		}
-		matched, err := regexp.MatchString(`.*\.pdf`, got)
+		matched, err := regexp.MatchString(`https://.*\.pdf`, got)
 		if err != nil {
 			t.Fatal(err)
 		}
